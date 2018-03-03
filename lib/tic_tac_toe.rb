fc8545c8d213ef60dec8aae96c6a0ee7686fcace
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-03 05:35:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,9 +47,10 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(index)
-      move(index, token = "X")
+      move(index, current_player)
+      display_board
     else
       turn
     end
-  end
+  end 
 end
