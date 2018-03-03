11fb434a967754157370a10aee337299e8f0ba22
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-03 05:59:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -101,4 +101,15 @@ class TicTacToe
     combo = won?
     @board[combo[0]] if(combo)
   end
+
+def play
+  until over?
+    turn 
+  end
+  if won? 
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end 
+ end
 end
